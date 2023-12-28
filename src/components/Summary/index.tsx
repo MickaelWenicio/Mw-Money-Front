8ed57810e787ae8556fb04a95267a28c74ac5be1
Mickael Wenicio <mickael.programador123@gmail.com>
2023-12-28 16:40:20 -0300

    feat: calculating deposits & withdraws in summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,8 +6,24 @@ import { useTransactions } from '../../hooks/useTransactions'
 
 const Summary = () => {
   const {transactions} = useTransactions()
+
   console.log(transactions)
 
+  const summary = transactions.reduce((acc, transaction)=>{
+    if(transaction.type === 'income'){
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount
+    } else {
+      acc.expenses += transaction.amount
+      acc.total -= transaction.amount
+    }
+
+    return acc
+  },{
+    deposits: 0,
+    expenses: 0,
+    total: 0 
+  })
 
   return (
     <Container>
@@ -16,21 +32,35 @@ const Summary = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{
+          new Intl.NumberFormat('pt-BR',{
+            style: 'currency',
+            currency:'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={expenseImg} alt="Saídas" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>{
+          new Intl.NumberFormat('pt-BR',{
+            style: 'currency',
+            currency:'BRL'
+          }).format(summary.expenses)}
+        </strong>
       </div>
       <div className='highlight-background'>
         <header>
           <p>Total</p>
           <img src={total} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>{
+          new Intl.NumberFormat('pt-BR',{
+            style: 'currency',
+            currency:'BRL'
+          }).format(summary.total)}</strong>
       </div>
     </Container>
   )
